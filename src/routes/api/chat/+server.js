8ecf3cd76238cfdf5ccc4e44f7c5595c3e21eb18
bluepuma77 <bluepuma77@gmail.com>
2024-01-26 16:55:35 +0100

    use new SSE locking

--- src/routes/api/chat/+server.js
+++ src/routes/api/chat/+server.js
@@ -1,55 +1,45 @@
-import { v4 as uuidv4 } from 'uuid'
-import { json } from '@sveltejs/kit'
 import { event } from 'sveltekit-sse'
+import { json } from '@sveltejs/kit'
 import { getRoomUsers, addRoomUser, delRoomUser, newMessage } from '$lib/server/chats.js'
 import { publish, subscribe } from '$lib/server/pubsub.js'
 
-const emitters = new Map();
-/* 
-
-// subscribe to new users
-subscribe('newUser', function emitter(data) {
-    console.log('emit users')
-    emitters.forEach(function run(emitter) {
-        if (emitter.room == data.room) {
-            emitter.emit('users', JSON.stringify(getRoomUsers(data.room)))
-        }
-    });
-})
- */
-
 // SSE function to stream back user list and new messages
 export function GET({ url }) {
+    let unsubscribeUsers
+    let unsubscribeMessage
+
     const name = url.searchParams.get('name')
     const room = url.searchParams.get('room')
-    console.log('GET SSE chat', name, room)
     addRoomUser(room, name)
+    console.log('getRoomUsers', room, getRoomUsers(room))
 
-    const id = new uuidv4()
+    // create SSE
     const e = event(async function run(emit) {
-        console.log('run')
-        emitters.set(id, { room, name, emit })
-        while (true) {
-            emit('users', 'x')
-            await new Promise(resolve => setTimeout(resolve, 5 * 1000))
-        }
-    }).onCancel(function stop() {
-        console.log('cancel')
+
+        // emit data on connect
+        emit(JSON.stringify(getRoomUsers(room)))
+        emit('test', JSON.stringify(getRoomUsers(room)))
+        emit('users', JSON.stringify(getRoomUsers(room)))
+
+        // emit data of subscription
+        await new Promise(function run(stop) {
+            unsubscribeUsers = subscribe('users-' + room, (data) => { emit('users', JSON.stringify(getRoomUsers(room))) })
+            unsubscribeMessage = subscribe('message-' + room, (data) => { emit('message', JSON.stringify(data)) })
+        })
+
+    }).onCancel(function cancel() {
+        // when connection is terminated
+        if (unsubscribeUsers) unsubscribeUsers()
+        if (unsubscribeMessage) unsubscribeMessage()
         delRoomUser(room, name)
-        emitters.delete(id)
     })
+
     return e.toResponse()
 }
 
-// POST to receive new chat input { room, name, text }
+// POST to receive new message
 export const POST = async ({ request }) => {
-    const data = await request.json();
-    console.log('POST', data)
-    emitters.forEach(function run(emitter) {
-        if (emitter.room == data.room) {
-            console.log('emit message', data)
-            emitter.emit('message', JSON.stringify(data))
-        }
-    })
-    return json(data)
+    const { room, name, text } = await request.json();
+    newMessage(room, name, text)
+    return json({ room, name, text })
 }
--- src/routes/api/rooms/+server.js
+++ src/routes/api/rooms/+server.js
@@ -1,31 +1,25 @@
-import { v4 as uuidv4 } from 'uuid';
 import { event } from 'sveltekit-sse'
 import { getRooms } from '$lib/server/chats.js'
-import { subscribe } from '$lib/server/pubsub.js'
+import { publish, subscribe } from '$lib/server/PubSub.js'
 
-const emitters = new Map();
-
-// subscribe to room updates
-subscribe('updateRooms', function emitter() {
-    emitters.forEach(function run(emit) {
-        emit(JSON.stringify(getRooms()))
-    });
-})
-
-// emit full list of rooms as string array
 export function GET() {
-    console.log('rooms')
-    const id = new uuidv4()
+    let unsubscribe
+
+    // create SSE
     const e = event(async function run(emit) {
-        console.log('run')
-        emitters.set(id, emit);
-        while (true) {
-            emit(JSON.stringify(getRooms()))
-            await new Promise(resolve => setTimeout(resolve, 60 * 1000))
-        }
-    }).onCancel(function stop() {
-        console.log('cancel');
-        emitters.delete(id);
+        
+        // emit data on connect
+        emit(JSON.stringify(getRooms()))
+
+        // emit data on subscription
+        await new Promise(function run(stop) {
+            unsubscribe = subscribe('rooms', (data) =>  { emit(JSON.stringify(getRooms())) })
+        })
+
+    }).onCancel(function cancel() {
+        // when connection is terminated
+        if (unsubscribe) unsubscribe()
     })
+
     return e.toResponse()
 }
