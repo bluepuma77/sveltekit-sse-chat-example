28fe9ed70f512c08d7c1c68439dc22a726865131
bluepuma77 <bluepuma77@gmail.com>
2024-01-26 16:54:55 +0100

    new pubsub

--- src/lib/server/pubsub.js
+++ src/lib/server/pubsub.js
@@ -1,16 +1,42 @@
 import { EventEmitter } from 'events';
 
-// Create an event emitter instance
-const eventEmitter = new EventEmitter();
+/**
+ * The PubSub class extends EventEmitter to provide publish-subscribe functionality.
+ * It allows clients to subscribe to events and publish messages to those events.
+ */
+class PubSub extends EventEmitter {
+    constructor() {
+        super();
+        // Increase the maximum number of listeners
+        this.setMaxListeners(1000);
 
-// Publish an event
-export function publish(eventName, data) {
-  console.log('publish', eventName, data)
-  eventEmitter.emit(eventName, data);
+        // Bind methods to the instance
+        this.subscribe = this.subscribe.bind(this);
+        this.publish = this.publish.bind(this);
+    }
+
+    /**
+     * Subscribes to a specific event with a listener.
+     * @param {string} eventName - The name of the event to subscribe to.
+     * @param {Function} listener - The callback function to execute when the event is emitted.
+     * @returns {Function} A function to unsubscribe the listener from the event.
+     */
+    subscribe(eventName, listener) {
+        this.on(eventName, listener);
+        return () => this.off(eventName, listener);
+    }
+
+    /**
+     * Publishes an event with the given data.
+     * @param {string} eventName - The name of the event to emit.
+     * @param {*} data - The data to pass to the event listeners.
+     */
+    publish(eventName, data) {
+        this.emit(eventName, data);
+    }
 }
 
-// Subscribe to an event
-export function subscribe(eventName, callback) {
-  console.log('subscribe', eventName)
-  eventEmitter.on(eventName, callback);
-}
+const pubSubInstance = new PubSub();
+
+export const subscribe = pubSubInstance.subscribe;
+export const publish = pubSubInstance.publish;
